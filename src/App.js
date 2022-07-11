b414c8c9d95c5a84639aa4c9274dd194f659bec6
Amogh N Rao <amogh090502@gmail.com>
2022-07-11 21:00:48 +0530

    Created state for App component

--- src/App.js
+++ src/App.js
@@ -5,31 +5,30 @@ import './App.css';
 
 
 class App extends Component{
+
+  constructor(){
+    super();
+
+    this.state = {
+      name: 'Amogh'
+    }
+  }
+
   render(){
     return (
       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi {this.state.name}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>
+          Change Name
+        </button>
       </header>
     </div>
     )
   }
 }
-// function App() {
-//   return (
-    
-//   );
-// }
 
 export default App;
