bb63c32715b49b018af492fecdefd1a4c85893f3
Amogh N Rao <amogh090502@gmail.com>
2022-07-11 23:45:32 +0530

    complete functionslity implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 
@@ -10,28 +9,44 @@ class App extends Component{
     super();
 
     this.state = {
-
-      monsters: [
-        {
-          name: 'Linda'
-        },
-        {
-          name: 'Frank'
-        },
-        {
-          name: 'Jacky'
-        }
-      ]
-      
+      monsters: [],
+      searchField: ''
     }
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState(() => {
+        return {monsters: users}
+      }, ()=>{
+        // console.log(this.state)
+      }))
+  }
+
   render(){
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+    })
+
     return (
       <div className="App">
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
+          const searchField = event.target.value.toLocaleLowerCase()
+          
+          this.setState(()=>{
+            return { searchField }
+          })
+          
+        }}/>
         {
-          this.state.monsters.map((monster) => {
-            return <h1>{monster.name}</h1>
+          filteredMonsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            )
           })
         }
       </div>
