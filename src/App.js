0babd3561d4e6ae68723b7f9465c8bf1f67931e9
Amogh N Rao <amogh090502@gmail.com>
2022-07-12 11:46:09 +0530

    Created components for card and search box

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
-
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 
@@ -35,7 +36,7 @@ class App extends Component {
 
 
   render() {
-
+    console.log('render from app')
     const { monsters, searchField} = this.state
     const { onSearchChange } = this
 
@@ -45,16 +46,15 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder='search monsters' onChange={onSearchChange} />
-        {
-          filteredMonsters.map((monster) => {
-            return (
-              <div key={monster.id}>
-                <h1>{monster.name}</h1>
-              </div>
-            )
-          })
-        }
+        
+        <SearchBox 
+          onChangeHandler = {onSearchChange} 
+          placeholder='search monsters' 
+          className='search-box'
+        />
+
+        <CardList monsters={filteredMonsters}/>
+        
       </div>
     )
   }
